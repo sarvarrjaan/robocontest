730bdf573a900c34d0fb296035f7cac1a06d7b7b
Sarvar <124452126+sarvarrjaan@users.noreply.github.com>
2023-02-26 14:54:41 +0500

    Update 0008.py
    Minimum maximum yig'indi

--- 0008.py
+++ 0008.py
@@ -1,2 +1,8 @@
+"""
+robocontest.uz #Minimum_maximum_yig'indi
+
+instagram.com/sarvarrjaan   #follow_me
+"""
+
 arr = list(map(int, input().split()))
 print(sum(arr) - max(arr), sum(arr) - min(arr))
