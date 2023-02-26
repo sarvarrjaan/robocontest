d40cbdd35509230d94d4669e30aeb43754311951
Sarvar <124452126+sarvarrjaan@users.noreply.github.com>
2023-02-26 14:52:24 +0500

    Update 0006.py
    Dasturchilar kuni #1

--- 0006.py
+++ 0006.py
@@ -1,3 +1,9 @@
+"""
+robocontest.uz #Dasturchilar kuni
+
+instagram.com/sarvarrjaan   #follow_me
+"""
+
 year = int(input())
 years = str('0'*(4-len(str(year))))+str(year)
 if year%400 == 0 or (year%4 == 0 and year%100 != 0):
